79c1ad12868e3e94358e883cae651681944139e5
Radivan <radivan100@gmail.com>
2026-09-21 22:59:13 +0700

    Inisiasi proyek pokemon-api-react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./Style.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-500">halo</h1>
+    </>
+  );
+}
+
+export default App;
